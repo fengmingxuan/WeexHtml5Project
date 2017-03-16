9b28fbf3bd0ec4a18a5b28686c043d30ed2ced52
Xenon Chau <cto@ido-game.com>
2017-03-16 16:09:39 +0800

    weex-html5

---
+++ src/main.js
@@ -0,0 +1,29 @@
+// import the original weex-html5.
+import weex from 'weex-html5'
+import hello from 'weex-hello'
+// install the component.
+weex.install(hello)
+
+// init the weex and load the bundle of 'demo/index.we'.
+; (function () {
+  function getUrlParam (key) {
+    const reg = new RegExp('[?|&]' + key + '=([^&]+)')
+    const match = location.search.match(reg)
+    return match && match[1]
+  }
+
+  const loader = getUrlParam('loader') || 'xhr'
+  const page = getUrlParam('page') || 'dist/mainlist.js'
+
+  // jsonp callback name should be specified or be the default
+  // value 'weexJsonpCallback' if the 'jsonp' loader is used.
+  const JSONP_CALLBACK_NAME = 'weexJsonpCallback'
+
+  weex.init({
+    jsonpCallback: JSONP_CALLBACK_NAME,
+    appId: location.href,
+    loader: loader,
+    source: page,
+    rootId: 'weex'
+  })
+})()
---
+++ src/taoguba.js
@@ -0,0 +1,80 @@
+var BASE_URL = {
+    IP:'192.168.1.15',
+    HTTP:'http://',
+    PORT:'12580',
+};
+
+var FIX = {
+    com:'https://api.taoguba.com.cn',
+    token:'web_4132&A99FC8CEC59DACE59AF20DE1E808DF21',
+};
+
+
+var API = {
+    apiGetForums:'/free/topic/apiGetForums?blockID=1&pageNo=1&flag=0',
+};
+
+
+exports.getDefaultUrl = function (name) {
+    var url;
+    url = getBaseUrl(name,true)+name+".js";
+    console.log('getDefaultUrl=='+url);
+    return url;
+};
+
+exports.getPathUrl = function (path) {
+    var url;
+    url = getBaseUrl(path,true)+path;
+    console.log('getPathUrl=='+url);
+    return url;
+};
+
+
+exports.getToken = function () {
+    var token;
+    token = FIX.token;
+    console.log('getToken=='+token);
+    return token;
+};
+
+
+exports.apiGetForums = function () {
+    var url;
+    url = FIX.com+ API.apiGetForums;
+    console.log('apiGetForums=='+url);
+    return url;
+};
+
+
+function getBaseUrl(bundleUrl, isnav) {
+    bundleUrl = new String(bundleUrl);
+    var nativeBase;
+    var isAndroidAssets = bundleUrl.indexOf('file://assets/') >= 0;
+
+    var isiOSAssets = bundleUrl.indexOf('file:///') >= 0 && bundleUrl.indexOf('WeexDemo.app') > 0;
+    if (isAndroidAssets) {
+        nativeBase = 'file://assets/dist/';
+    }
+    else if (isiOSAssets) {
+        nativeBase = bundleUrl.substring(0, bundleUrl.lastIndexOf('/') + 1);
+    }
+    else {
+        //'localhost:8080';
+        var host = BASE_URL.IP+':'+BASE_URL.PORT;
+        var matches = /\/\/([^\/]+?)\//.exec(bundleUrl);
+        if (matches && matches.length >= 2) {
+            host = matches[1];
+        }
+
+        //此处需注意一下,tabbar 用的直接是jsbundle 的路径,但是navigator是直接跳转到新页面上的.
+        //网页 http://localhost:8080/index.html?page=./dist/weexbar/stocknews.js
+        //android 原生 http://192.168.1.15:12580/dist/mainlist.js
+        if (typeof window === 'object') {
+            nativeBase = isnav ? 'http://' + host + '/index.html?page=./dist/' : '/dist/';
+        } else {
+            nativeBase = 'http://' + host + '/dist/';
+        }
+    }
+
+    return nativeBase;
+};
